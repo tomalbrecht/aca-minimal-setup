7fb1f56818a19b20922869604af1bfd79f5f73cd
Thomas Albrecht <thomas.albrecht@bival.de>
2024-06-26 16:01:04 +0200

    add: w/o managed identity

---
+++ README.md
@@ -0,0 +1,91 @@
+## Local Setup
+
+- Install VSCode
+- [Install Azure CLI](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli)
+- Run the 
+
+## Azure Deployment (without Managed Identity)
+> Microsoft recommends a deployment with managed identity!
+
+
+### Set configurations
+```zsh
+RESOURCE_GROUP="rg-aca-album-dev"
+LOCATION="westeurope"
+ACA_ENVIRONMENT="aca-env-album"
+ACA_BACKEND_API="album-api"
+ACA_FRONTEND_UI="album-ui"
+ACR_NAME="acracaalbums0137"
+```
+
+### Create resource group
+```zsh
+az group create \
+         --name $RESOURCE_GROUP \
+         --location $LOCATION
+
+az acr create \
+       --resource-group $RESOURCE_GROUP \
+       --name $ACR_NAME \
+       --sku Basic \
+       --admin-enabled true
+```
+
+### Build backend image
+```zsh
+az acr build --registry $ACR_NAME \
+       --image $ACA_BACKEND_API ./aca-albumapi-python/src
+```
+
+### Create environment
+```zsh
+az containerapp env create \
+                --name $ACA_ENVIRONMENT \
+                --resource-group $RESOURCE_GROUP \
+                --location $LOCATION
+```
+
+### Create backend container
+```zsh
+az containerapp create \
+                --name $ACA_BACKEND_API \
+                --resource-group $RESOURCE_GROUP \
+                --environment $ACA_ENVIRONMENT \
+                --image $ACR_NAME'.azurecr.io/'$ACA_BACKEND_API \
+                --target-port 8080 \
+                --ingress 'internal' \
+                --registry-server $ACR_NAME'.azurecr.io' \
+                --query properties.configuration.ingress.fqdn
+```
+
+### Build frontend image
+```zsh
+az acr build --registry $ACR_NAME --image $ACA_FRONTEND_UI ./aca-frontend-js/
+```
+
+###
+```zsh
+API_BASE_URL=$(az containerapp show --resource-group $RESOURCE_GROUP --name $ACA_BACKEND_API --query properties.configuration.ingress.fqdn -o tsv)
+echo $API_BASE_URL
+# album-api.internal.purplepond-4fe75b66.westeurope.azurecontainerapps.io
+```
+
+
+## Misc
+
+### Create new revision of the backend container
+```zsh
+az containerapp update \
+                --name $ACA_BACKEND_API \
+                --resource-group $RESOURCE_GROUP \
+                --image $ACR_NAME'.azurecr.io/'$ACA_BACKEND_API \
+                --query properties.configuration.ingress.fqdn
+```
+
+### Delete the backend container
+```zsh
+az containerapp delete \
+                --name $ACA_BACKEND_API \
+                --resource-group $RESOURCE_GROUP  
+```
+
--- acr-without-mi.sh
+++ acr-without-mi.sh
@@ -11,44 +11,45 @@ $ACA_BACKEND_API="album-api"
 $ACA_FRONTEND_UI="album-ui"
 $ACR_NAME="acracaalbums0136"
 
-az group create `
-         --name $RESOURCE_GROUP `
+az group create \
+         --name $RESOURCE_GROUP \
          --location $LOCATION
 
-az acr create `
-       --resource-group $RESOURCE_GROUP `
-       --name $ACR_NAME `
-       --sku Basic `
+az acr create \
+       --resource-group $RESOURCE_GROUP \
+       --name $ACR_NAME \
+       --sku Basic \
        --admin-enabled true
 
 #az acr build --registry $ACR_NAME --image $ACA_BACKEND_API ../backend_api/backend_api_python/
-az acr build --registry $ACR_NAME --image $ACA_BACKEND_API ./containerapps-albumapi-python/src
+az acr build --registry $ACR_NAME \
+       --image $ACA_BACKEND_API ./aca-albumapi-python/src
 
-az containerapp env create `
-                --name $ACA_ENVIRONMENT `
-                --resource-group $RESOURCE_GROUP `
+az containerapp env create \
+                --name $ACA_ENVIRONMENT \
+                --resource-group $RESOURCE_GROUP \
                 --location $LOCATION
 
-az containerapp create `
-                --name $ACA_BACKEND_API `
-                --resource-group $RESOURCE_GROUP `
-                --environment $ACA_ENVIRONMENT `
-                --image $ACR_NAME'.azurecr.io/'$ACA_BACKEND_API `
-                --target-port 8080 `
-                --ingress 'internal' `
-                --registry-server $ACR_NAME'.azurecr.io' `
+az containerapp create \
+                --name $ACA_BACKEND_API \
+                --resource-group $RESOURCE_GROUP \
+                --environment $ACA_ENVIRONMENT \
+                --image $ACR_NAME'.azurecr.io/'$ACA_BACKEND_API \
+                --target-port 8080 \
+                --ingress 'internal' \
+                --registry-server $ACR_NAME'.azurecr.io' \
                 --query properties.configuration.ingress.fqdn
 
 ## neue Revision erstellen
-az containerapp update `
-                --name $ACA_BACKEND_API `
-                --resource-group $RESOURCE_GROUP `
-                --image $ACR_NAME'.azurecr.io/'$ACA_BACKEND_API `
+az containerapp update \
+                --name $ACA_BACKEND_API \
+                --resource-group $RESOURCE_GROUP \
+                --image $ACR_NAME'.azurecr.io/'$ACA_BACKEND_API \
                 --query properties.configuration.ingress.fqdn
 
 ## Löschen
-# az containerapp delete `
-#                 --name $ACA_BACKEND_API `
+# az containerapp delete \
+#                 --name $ACA_BACKEND_API \
 #                 --resource-group $RESOURCE_GROUP  
 
 # Vorher Bugfixen der package.json!
@@ -56,18 +57,18 @@ az containerapp update `
 #az acr build --registry $ACR_NAME --image $ACA_FRONTEND_UI ../frontend_ui/
 az acr build --registry $ACR_NAME --image $ACA_FRONTEND_UI ./aca-course/frontend_ui/
 
-$API_BASE_URL=$(az containerapp show --resource-group $RESOURCE_GROUP --name $ACA_BACKEND_API --query properties.configuration.ingress.fqdn -o tsv)
+API_BASE_URL=$(az containerapp show --resource-group $RESOURCE_GROUP --name $ACA_BACKEND_API --query properties.configuration.ingress.fqdn -o tsv)
 echo $API_BASE_URL
 # album-api.internal.purplepond-4fe75b66.westeurope.azurecontainerapps.io
 
-az containerapp create `
-  --name $ACA_FRONTEND_UI `
-  --resource-group $RESOURCE_GROUP `
-  --environment $ACA_ENVIRONMENT `
-  --image $ACR_NAME'.azurecr.io/'$ACA_FRONTEND_UI  `
-  --target-port 3000 `
-  --env-vars API_BASE_URL=https://$API_BASE_URL `
-  --ingress 'external' `
-  --registry-server $ACR_NAME'.azurecr.io' `
+az containerapp create \
+  --name $ACA_FRONTEND_UI \
+  --resource-group $RESOURCE_GROUP \
+  --environment $ACA_ENVIRONMENT \
+  --image $ACR_NAME'.azurecr.io/'$ACA_FRONTEND_UI  \
+  --target-port 3000 \
+  --env-vars API_BASE_URL=https://$API_BASE_URL \
+  --ingress 'external' \
+  --registry-server $ACR_NAME'.azurecr.io' \
   --query properties.configuration.ingress.fqdn
 # "album-ui.purplepond-4fe75b66.westeurope.azurecontainerapps.io"
